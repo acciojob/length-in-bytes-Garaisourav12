e02a1a69286d4ffa37b956d8e0170f3cc207e43a
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-10-29 00:18:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	const blob = new Blob([str]);
+
+  // Get the size property of the Blob to retrieve the byte size
+  return blob.size;
 };
 
 // Do not change the code below
-// const str = prompt("Enter some string.");
-// alert(byteSize(str));
+const str = prompt("Enter some string.");
+alert(byteSize(str));
